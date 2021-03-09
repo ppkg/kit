d063da5a1af46bf91f118100c4aad70fc717fe4a
duyan <duyan@rp-pet.cn>
2021-03-09 08:58:45 +0800

    更新分转元，元转分方法

--- kit.go
+++ kit.go
@@ -44,20 +44,34 @@ func CatchPanic() {
 }
 
 //元转分
-func YuanToFen(f float64) int {
-	decimalValue := decimal.NewFromFloat(f)
-	decimalValue = decimalValue.Mul(decimal.NewFromInt(100))
-	res, _ := decimalValue.Float64()
+func YuanToFen(f interface{}) int {
+	var decimalValue decimal.Decimal
+	switch f.(type) {
+	case float32:
+		decimalValue = decimal.NewFromFloat32(f.(float32))
+		decimalValue = decimalValue.Mul(decimal.NewFromInt(100))
+	case float64:
+		decimalValue = decimal.NewFromFloat(f.(float64))
+		decimalValue = decimalValue.Mul(decimal.NewFromInt(100))
+	}
 
+	res, _ := decimalValue.Float64()
 	return int(res)
 }
 
 //分转元
-func FenToYuan(i int64) float64 {
-	decimalValue := decimal.NewFromInt(i)
-	decimalValue = decimalValue.Div(decimal.NewFromInt(100))
-	res, _ := decimalValue.Float64()
+func FenToYuan(i interface{}) float64 {
+	var decimalValue decimal.Decimal
+	switch i.(type) {
+	case int32:
+		decimalValue = decimal.NewFromInt32(i.(int32))
+		decimalValue = decimalValue.Div(decimal.NewFromInt32(100))
+	case int64:
+		decimalValue = decimal.NewFromInt(i.(int64))
+		decimalValue = decimalValue.Div(decimal.NewFromInt(100))
+	}
 
+	res, _ := decimalValue.Float64()
 	return res
 }
 
